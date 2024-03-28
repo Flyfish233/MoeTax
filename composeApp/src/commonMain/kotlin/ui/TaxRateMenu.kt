1314fdfa8c58e6750afb28e73dbda342d58c023c
Flyfish233 <37693645+Flyfish233@users.noreply.github.com>
2024-03-29 00:28:48 +0800

    Code cleanup

--- composeApp/src/commonMain/kotlin/ui/TaxRateMenu.kt
+++ composeApp/src/commonMain/kotlin/ui/TaxRateMenu.kt
@@ -1,9 +1,6 @@
 package ui
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
